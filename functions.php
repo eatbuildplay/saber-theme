0b7b2b7b75fa7432407c9c3a2eca2d9612056fd4
Casey Milne <joel@goldhat.ca>
2020-05-15 17:10:44 -0500

    edited functions

---
+++ functions.php
@@ -0,0 +1,44 @@
+<?php
+
+function saberRegisterMenu() {
+  register_nav_menu('header-menu',__( 'Header Menu' ));
+  register_nav_menu('footer-menu',__( 'Footer Menu' ));
+}
+add_action( 'init', 'saberRegisterMenu' );
+
+if( function_exists('acf_add_options_page') ) {
+
+  acf_add_options_page(array(
+		'page_title' 	=> 'Theme General Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Header Settings',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Theme Footer Settings',
+		'menu_title'	=> 'Footer',
+		'parent_slug'	=> 'theme-general-settings',
+	));
+
+}
+
+
+add_action('wp_enqueue_scripts', 'saberScripts');
+function saberScripts() {
+
+  wp_enqueue_style(
+    'saber-style',
+    get_template_directory_uri() . '/style.css',
+    array(),
+    true
+  );
+
+}
