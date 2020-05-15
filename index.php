7eaabb762a108789860de7df8472e5d1da95da97
Casey Milne <joel@goldhat.ca>
2020-05-15 17:04:45 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ *
+ * The main template file
+ *
+ *
+ */
+
+get_header();
+
+?>
+
+<main id="site-content" role="main">
+
+<?php
+if ( have_posts() ) :
+	while ( have_posts() ):
+		the_post();
+    the_content();
+	endwhile;
+endif;
+?>
+
+</main>
+
+<?php
+get_footer();
---
+++ src/core/Loader.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SaberTheme;
+
+class Loader {
+
+
+
+}
